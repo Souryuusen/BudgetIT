5c3750b1c052fec25c8910d28d752498a7608d2a
Souryuusen <16085670+souryuusen@users.noreply.github.com>
2025-01-02 12:59:49 +0100

    Initial Commit

---
+++ src/main/java/com/soursoft/budgetit/BudgetItApplication.java
@@ -0,0 +1,13 @@
+package com.soursoft.budgetit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetItApplication.class, args);
+    }
+
+}
